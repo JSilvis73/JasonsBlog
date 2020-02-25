94210305829d989678c38c9afe30a39c8786a53c
Jason <JSilvis030592@yahoo.com>
2020-02-24 19:19:51 -0500

    Added Models folder and medication class. Also added a context file and connection string

---
+++ JasonsBlog/Models/Medication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JasonsBlog.Models
+{
+    public class Medication
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
