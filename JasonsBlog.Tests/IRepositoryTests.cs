0cc98a4d24e5e9750e18141226c5ef556a28184e
Jason <JSilvis030592@yahoo.com>
2020-02-27 19:00:55 -0500

    Added Irepository and Irepositorytests class

---
+++ JasonsBlog.Tests/IRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JasonsBlog.Tests
+{
+    public class IRepositoryTests
+    {
+        [Fact]
+        public void TODO()
+        {
+            Assert.True(false);
+        }
+    }
+}
